fe2c19381b9f369fc79123c06a5178ba5799457e
arihant-jain0 <jainari1208@gmail.com>
2024-08-03 14:13:14 +0530

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "@/sections/Footer";
+import { Hero } from "@/sections/Hero";
+
+export default function Home() {
+  return <div>
+
+
+    <Hero/>
+    <Footer/>
+  </div>;
+}
