7d8065dd50a46e3f0d2410c999476b902c05fc62
arihant-jain0 <jainari1208@gmail.com>
2024-08-04 09:07:30 +0530

    refactor: Update layout.tsx to improve body element structure
    - Remove duplicate body element and merge its className with the existing body element
    - Add missing closing tag for the body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <body className={`inter.className bg-black text-white` }>
       <Analytics/>
-      <body className={`inter.className bg-black text-white` }>{children}</body>
+        
+        {children}</body>
       
     </html>
   );
