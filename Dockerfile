de371f6f2f211aa64f88862dffe606059d1ac9ab
itzamanjain <aman.044063@tmu.ac.in>
2024-08-12 22:54:22 +0530

    added ratelimit and docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:20-alpine
+
+WORKDIR /usr/src/app
+
+COPY . .
+RUN npm install
+RUN npm run build
+CMD ["npm", "start"]
--- src/app/api/v1/report/route.ts
+++ src/app/api/v1/report/route.ts
@@ -4,8 +4,54 @@ import { connectDb } from "@/dbconfig/dbconfig";
 
 connectDb();
 
+
+
+// In-memory store for tracking requests
+const rateLimitStore = new Map();
+const RATE_LIMIT_TIME_FRAME = 15 * 60 * 1000; // 15 minutes
+const RATE_LIMIT_MAX_REQUESTS = 3; // Max 3 requests per IP per time frame
+
+function rateLimit(request) {
+    const ip = request.headers.get('x-forwarded-for') || request.ip ;
+    console.log('IP:', ip);
+    
+    const now = Date.now();
+
+    if (!rateLimitStore.has(ip)) {
+        rateLimitStore.set(ip, { count: 1, lastRequest: now });
+    } else {
+        const rateLimitData = rateLimitStore.get(ip);
+        if (now - rateLimitData.lastRequest > RATE_LIMIT_TIME_FRAME) {
+            // Reset the rate limit for the IP if the time frame has passed
+            rateLimitStore.set(ip, { count: 1, lastRequest: now });
+        } else {
+            // Increment the request count for the IP
+            rateLimitData.count++;
+            rateLimitData.lastRequest = now;
+
+            if (rateLimitData.count > RATE_LIMIT_MAX_REQUESTS) {
+                return false; // Rate limit exceeded
+            }
+        }
+    }
+
+    return true; // Within the rate limit
+}
+
+
+
+
 export async function POST(request: NextRequest) {
     try {
+
+        if (!rateLimit(request)) {
+            return NextResponse.json({
+                message: 'Too many requests. Please try again later.',
+                success: false
+            }, { status: 429 });
+        }
+
+
         const reqBody = await request.json();
         const { url, reportedBy, email, reason, captcha } = reqBody;
 
